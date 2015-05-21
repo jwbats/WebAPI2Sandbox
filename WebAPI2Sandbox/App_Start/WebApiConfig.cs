bdee6a0b2632d3a84a33fb427e60f6b2f8ed55e1
JayRenegade <jayrenegade@hotmail.com>
2015-05-21 22:59:36 +0200

    Persons controller added with {action} routing.

--- WebAPI2Sandbox/App_Start/WebApiConfig.cs
+++ WebAPI2Sandbox/App_Start/WebApiConfig.cs
@@ -5,20 +5,30 @@ using System.Web.Http;
 
 namespace WebAPI2Sandbox
 {
-    public static class WebApiConfig
-    {
-        public static void Register(HttpConfiguration config)
-        {
-            // Web API configuration and services
+	public static class WebApiConfig
+	{
+		public static void Register(HttpConfiguration config)
+		{
+			// Web API configuration and services
 
-            // Web API routes
-            config.MapHttpAttributeRoutes();
+			// Web API routes
+			config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-        }
-    }
+			// Place route with {action} on top, so it matches first.
+			config.Routes.MapHttpRoute(
+				name: "PersonsApi",
+				routeTemplate: "api/{controller}/{action}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+			
+			// Follow up with route that does not contain {action}.
+			config.Routes.MapHttpRoute(
+				name: "ProductsApi",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+
+			
+		}
+	}
 }
